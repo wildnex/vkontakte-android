2187fe8159691baeeae020bab19bac813c97f81c
ildar.karimov <ildar.karimov@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2009-08-25 23:37:06 +0000

    added photo and new friends counter

--- trunk/src/org/googlecode/vkontakte_android/CFriendsTab.java
+++ trunk/src/org/googlecode/vkontakte_android/CFriendsTab.java
@@ -3,6 +3,13 @@ package org.googlecode.vkontakte_android;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.AbsListView;
+import android.widget.TextView;
+import android.view.View;
+
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.googlecode.userapi.VkontakteAPI;
 
 public class CFriendsTab extends ListActivity implements AbsListView.OnScrollListener {
     private FriendsListAdapter adapter;
@@ -15,6 +22,16 @@ public class CFriendsTab extends ListActivity implements AbsListView.OnScrollLis
         adapter = new FriendsListAdapter(this, R.layout.friend_row);
         setListAdapter(adapter);
         getListView().setOnScrollListener(this);
+        TextView tv = (TextView) findViewById(R.id.new_counter);
+        try {
+            long counter = CGuiTest.api.getFriends(CGuiTest.api.id, 0, 0, VkontakteAPI.friendsTypes.friends_new).getCount();
+            tv.setText("new friends: "+counter);
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (JSONException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
     }
 
     public void onScroll(AbsListView v, int i, int j, int k) {
