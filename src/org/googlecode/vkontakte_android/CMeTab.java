71fb108f9e2afc9470a5d39a332f1077f73e7fca
ildar.karimov <ildar.karimov@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2009-08-26 09:16:27 +0000

    added new updates counter

--- src/org/googlecode/vkontakte_android/CMeTab.java
+++ src/org/googlecode/vkontakte_android/CMeTab.java
@@ -6,6 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TableLayout;
+import android.widget.TextView;
+import static org.googlecode.vkontakte_android.R.id.updates_counter;
+import org.googlecode.userapi.VkontakteAPI;
+import org.googlecode.userapi.ChangesHistory;
+import org.json.JSONException;
+
+import java.io.IOException;
 
 public class CMeTab extends Activity {
 
@@ -14,7 +21,18 @@ public class CMeTab extends Activity {
 		super.onCreate(savedInstanceState);
 		
         setContentView(R.layout.main);
-               
+
+        TextView updates = (TextView) findViewById(updates_counter);
+        VkontakteAPI api = CGuiTest.api;
+        try {
+            ChangesHistory history = api.getChangesHistory();
+            updates.setText("messages: "+history.getMessagesCount()+", friends: "+history.getFriendsCount()+", photos: "+history.getPhotosCount());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();  
+        }
+
         ImageButton b = (ImageButton) findViewById(R.id.ImageButton01);
         b.setImageDrawable(new BitmapDrawable(CImagesManager.getBitmap("ok")));
         
