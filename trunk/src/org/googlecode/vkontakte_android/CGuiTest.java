cee913ac0ce9bf570a6ca68b3e6725b5f3708aa1
jecklandin <jecklandin@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2009-09-03 17:07:07 +0000

    log/pass saved in prefs. logout button was added

--- trunk/src/org/googlecode/vkontakte_android/CGuiTest.java
+++ trunk/src/org/googlecode/vkontakte_android/CGuiTest.java
@@ -38,8 +38,26 @@ public class CGuiTest extends TabActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //TODO handle UnknownHostException
+        //TODO handle JSONException in api methods
+        
         api = new VkontakteAPI();
-
+        if (CSettings.isLogged(this))
+        {
+        	try{
+        	Log.d("login","already logged. using existing log/pass");
+        	api.login(CSettings.getLogin(this), CSettings.getPass(this));
+        	initializeActivity(this);
+        	return;
+        	}
+        	catch (IOException ex)
+        	{
+        		//show toast and then login dialog
+        		Toast.makeText(getApplicationContext(), "Either login or password is incorrect", Toast.LENGTH_SHORT).show();
+        	}
+        }
+        
+        
         final LoginDialog ld = new LoginDialog(this);
         ((EditText) ld.findViewById(R.id.login)).setText("fake4test@gmail.com");
         ((EditText) ld.findViewById(R.id.pass)).setText("qwerty");
@@ -52,41 +70,8 @@ public class CGuiTest extends TabActivity {
                     Log.w(login, pass);
                     if (api.login(login, pass)) {
                         ld.dismiss();
-                        refresh();
-// load icons from the files
-                        CImagesManager.loadImages(CGuiTest.this);
-
-                        final TabHost tabHost = getTabHost();
-
-                        tabHost.addTab(tabHost.newTabSpec("I")
-                                .setIndicator(getResources().getString(R.string.i))
-                                .setContent(new Intent(CGuiTest.this, CMeTab.class)));
-
-                        tabHost.addTab(tabHost.newTabSpec("Friends")
-                                .setIndicator(getResources().getString(R.string.friends), getResources().getDrawable(R.drawable.ic_menu_friendslist))
-                                .setContent(new Intent(CGuiTest.this, CFriendsTab.class)));
-
-
-                        tabHost.addTab(tabHost.newTabSpec("Messages")
-                                .setIndicator(getResources().getString(R.string.messages))
-                                .setContent(new Intent(CGuiTest.this, CMessagesTab.class)));
-
- 
-                        //todo: remove - just P-o-C here
-                        final TextView tv = TabHelper.injectTabCounter(getTabWidget(), 2, getApplicationContext());
-
-                        //todo: register/unregister onResume/onPause
-                        getContentResolver().registerContentObserver(UserapiProvider.USERS_URI, false, new ContentObserver(new Handler()) {
-                            @Override
-                            public void onChange(boolean b) {
-                                Cursor cursor = managedQuery(UserapiProvider.USERS_URI, null, null, null, null);//todo: change cursor to new only
-                                if (cursor.getCount() == 0) tv.setVisibility(View.INVISIBLE);
-                                else {
-                                    tv.setText(String.valueOf(cursor.getCount()));
-                                    tv.setVisibility(View.VISIBLE);
-                                }
-                            }
-                        });
+                        CSettings.saveLogin(CGuiTest.this, login, pass);
+                        initializeActivity(CGuiTest.this);
                     } else {
                         Toast.makeText(getApplicationContext(), "login/pass incorrect", Toast.LENGTH_SHORT).show();
                     }
@@ -99,6 +84,50 @@ public class CGuiTest extends TabActivity {
 
 
     }
+    
+    
+    private void initializeActivity(Context ctx)
+    {
+        refresh();
+        // load icons from the files
+		CImagesManager.loadImages(ctx);
+
+        final TabHost tabHost = getTabHost();
+
+		tabHost.addTab(tabHost.newTabSpec("I").setIndicator(
+				getResources().getString(R.string.i)).setContent(
+				new Intent(CGuiTest.this, CMeTab.class)));
+
+		tabHost.addTab(tabHost.newTabSpec("Friends").setIndicator(
+				getResources().getString(R.string.friends),
+				getResources().getDrawable(R.drawable.ic_menu_friendslist))
+				.setContent(new Intent(CGuiTest.this, CFriendsTab.class)));
+
+		tabHost.addTab(tabHost.newTabSpec("Messages").setIndicator(
+				getResources().getString(R.string.messages)).setContent(
+				new Intent(CGuiTest.this, CMessagesTab.class)));
+
+		// todo: remove - just P-o-C here
+		final TextView tv = TabHelper.injectTabCounter(getTabWidget(), 2,
+				getApplicationContext());
+
+		// todo: register/unregister onResume/onPause
+		getContentResolver().registerContentObserver(UserapiProvider.USERS_URI,
+				false, new ContentObserver(new Handler()) {
+					@Override
+					public void onChange(boolean b) {
+						Cursor cursor = managedQuery(UserapiProvider.USERS_URI,
+								null, null, null, null);// todo: change cursor
+														// to new only
+						if (cursor.getCount() == 0)
+							tv.setVisibility(View.INVISIBLE);
+						else {
+							tv.setText(String.valueOf(cursor.getCount()));
+							tv.setVisibility(View.VISIBLE);
+						}
+					}
+				});
+	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -117,6 +146,19 @@ public class CGuiTest extends TabActivity {
             case R.id.settings:
             	startActivity(new Intent(this, CSettings.class));
             	return true;
+            case R.id.logout:
+                try {
+					api.logout();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				stopService(new Intent(this, CheckingService.class));
+				CSettings.clearPrivateInfo(this);
+            	finish();
+                
+            	
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
