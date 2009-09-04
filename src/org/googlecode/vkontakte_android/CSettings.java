16c5aacbc97ab56309da9181a3734cbf55eab5c1
ildar.karimov <ildar.karimov@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2009-09-04 19:51:36 +0000

    some code clean/reformat

--- src/org/googlecode/vkontakte_android/CSettings.java
+++ src/org/googlecode/vkontakte_android/CSettings.java
@@ -4,84 +4,71 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceManager;
-import android.preference.PreferenceScreen;
-import android.util.Log;
+import android.preference.*;
 
-public class CSettings extends PreferenceActivity  implements Preference.OnPreferenceChangeListener
-{
+public class CSettings extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		addPreferencesFromResource(R.xml.preferences);
-		
-		PreferenceScreen scr = getPreferenceScreen();
-		Preference ps  =  scr.findPreference("sound");
-		ps.setOnPreferenceChangeListener(this);
-		
-		Preference pn  =  scr.findPreference("notif");
-		pn.setOnPreferenceChangeListener(this);
-		
-		Preference list  =  scr.findPreference("period");
-		list.setOnPreferenceChangeListener(this); 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
 
-		//SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		//Log.d("pr",prefs.getString("period", " "));
-		
-	}
+        PreferenceScreen scr = getPreferenceScreen();
+        Preference ps = scr.findPreference("sound");
+        ps.setOnPreferenceChangeListener(this);
 
-	@Override
-	public boolean onPreferenceChange(Preference arg0, Object arg1) {
-		String key = arg0.getKey();
-		
-		if (key.equals("period"))
-		{
-			ListPreference pr = ((ListPreference)arg0); 
-			int pos = pr.findIndexOfValue((String)arg1);
-			pr.setSummary(pr.getEntries()[ pos ]);
-		}
-		return true;
-	}
+        Preference pn = scr.findPreference("notif");
+        pn.setOnPreferenceChangeListener(this);
 
-	//================  work with login/pass
-	
-	public static void saveLogin(Context ctx, String login, String pass) {
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
-		Editor ed = prefs.edit();
-		ed.putString("login", login);
-		ed.commit();
-		ed.putString("password", pass);
-		ed.commit();
-	}
-	
-	public static boolean isLogged(Context ctx)
-	{
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
-		return prefs.contains("login") && prefs.contains("password");
-	}
-	
-	public static void clearPrivateInfo(Context ctx)
-	{
-		Editor ed = PreferenceManager.getDefaultSharedPreferences(ctx).edit();
-		ed.remove("login");
-		ed.commit();
-		ed.remove("password");
-		ed.commit();
-	}
-	
-	public static String getPass(Context ctx)
-	{
-		return PreferenceManager.getDefaultSharedPreferences(ctx).getString("password", "");
-	}
-	
-	public static String getLogin(Context ctx)
-	{
-		return PreferenceManager.getDefaultSharedPreferences(ctx).getString("login", "");
-	}
-	
+        Preference list = scr.findPreference("period");
+        list.setOnPreferenceChangeListener(this);
 
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Log.d("pr",prefs.getString("period", " "));
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference arg0, Object arg1) {
+        String key = arg0.getKey();
+
+        if (key.equals("period")) {
+            ListPreference pr = ((ListPreference) arg0);
+            int pos = pr.findIndexOfValue((String) arg1);
+            pr.setSummary(pr.getEntries()[pos]);
+        }
+        return true;
+    }
+
+    //================  work with login/pass
+
+    public static void saveLogin(Context ctx, String login, String pass) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
+        Editor ed = prefs.edit();
+        ed.putString("login", login);
+        ed.commit();
+        ed.putString("password", pass);
+        ed.commit();
+    }
+
+    public static boolean isLogged(Context ctx) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
+        return prefs.contains("login") && prefs.contains("password");
+    }
+
+    public static void clearPrivateInfo(Context ctx) {
+        Editor ed = PreferenceManager.getDefaultSharedPreferences(ctx).edit();
+        ed.remove("login");
+        ed.commit();
+        ed.remove("password");
+        ed.commit();
+    }
+
+    public static String getPass(Context ctx) {
+        return PreferenceManager.getDefaultSharedPreferences(ctx).getString("password", "");
+    }
+
+    public static String getLogin(Context ctx) {
+        return PreferenceManager.getDefaultSharedPreferences(ctx).getString("login", "");
+    }
 }
