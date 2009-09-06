82bacee75fa848e56c029f4f6f2478a8962ad149
a.kosenkov <a.kosenkov@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2009-09-06 16:06:23 +0000

    startup bugfix

--- trunk/src/org/googlecode/vkontakte_android/CSettings.java
+++ trunk/src/org/googlecode/vkontakte_android/CSettings.java
@@ -83,9 +83,8 @@ public class CSettings extends PreferenceActivity implements Preference.OnPrefer
     public static String getSid(Context ctx) {
         return PreferenceManager.getDefaultSharedPreferences(ctx).getString("sid", "");
     }
-    
-    public static int getPeriod(Context ctx)
-    {
-    	 return PreferenceManager.getDefaultSharedPreferences(ctx).getInt("period", 60);
+
+    public static int getPeriod(Context ctx) {
+        return Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(ctx).getString("period", "60"));
     }
 }
