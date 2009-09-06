6a74743ca62246e3844cc52c9bfd59c1c49df808
a.kosenkov <a.kosenkov@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2009-09-06 14:25:57 +0000

    ContactsMatcher ищет по имени

--- trunk/src/org/googlecode/vkontakte_android/matching/ContactsMatcher.java
+++ trunk/src/org/googlecode/vkontakte_android/matching/ContactsMatcher.java
@@ -4,8 +4,13 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.provider.BaseColumns;
 import android.provider.Contacts;
+import android.util.Log;
 import org.googlecode.vkontakte_android.contacts.ContactsHelper;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContactsMatcher {
     private final ContentResolver contentResolver;
@@ -33,14 +38,47 @@ public class ContactsMatcher {
             sb.append(Contacts.PhonesColumns.NUMBER_KEY + "=?");
         }
 
-        final Cursor q = contentResolver.query(Contacts.Phones.CONTENT_URI, new String[]{"_id"},
-                sb.toString(), filtered, null);
+        final Cursor q = contentResolver.query(Contacts.Phones.CONTENT_URI,
+                new String[]{Contacts.Phones.PERSON_ID},
+                sb.toString(),
+                filtered, null);
+        return ContactsHelper.createPersonUri(getUniqueRecord(q, Contacts.Phones.PERSON_ID));
+    }
 
+    public Uri findPersonByName(String name) {
+
+        final Cursor q = contentResolver.query(Contacts.People.CONTENT_URI,
+                new String[]{BaseColumns._ID},
+                Contacts.PeopleColumns.NAME + "=? or " + Contacts.PeopleColumns.DISPLAY_NAME + "=?",
+                new String[]{name, name},
+                null);
+
+        return ContactsHelper.createPersonUri(getUniqueRecord(q, Contacts.Phones.PERSON_ID));
+    }
+
+    private static String getUniqueRecord(Cursor q, String column) {
         try {
-
             if (!q.moveToFirst()) return null;
-            return ContactsHelper.createPersonUri(q.getString(q.getColumnIndexOrThrow("_id")));
+            final int idColumn = q.getColumnIndexOrThrow(column);
 
+            final int cnt = q.getCount();
+            if (cnt != 1) {
+                return q.getString(idColumn);
+            }
+
+            String lastId;
+            Set<String> persons = new HashSet<String>(cnt);  // слишком сложно
+            do {
+                lastId = q.getString(idColumn);
+                persons.add(lastId);
+            } while (q.moveToNext());
+
+            if (persons.size() > 1) {
+                Log.w("VK", "Several contacts found with unique filter: " + persons);
+                return null;
+            }
+
+            return lastId;
         } finally {
             q.close();
         }
