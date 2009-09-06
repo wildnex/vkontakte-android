081b300f4e8fb818577ca7bda78cee49b76d7177
ildar.karimov <ildar.karimov@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2009-09-06 16:05:18 +0000

    read/unread indicator

--- trunk/src/org/googlecode/vkontakte_android/MessagesListAdapter.java
+++ trunk/src/org/googlecode/vkontakte_android/MessagesListAdapter.java
@@ -9,7 +9,6 @@ import org.googlecode.vkontakte_android.database.MessageDao;
 
 
 public class MessagesListAdapter extends ResourceCursorAdapter {
-
     public MessagesListAdapter(Context context, int layout, Cursor cursor) {
         super(context, layout, cursor);
     }
@@ -18,8 +17,11 @@ public class MessagesListAdapter extends ResourceCursorAdapter {
     public void bindView(View view, Context context, Cursor cursor) {
         MessageDao messageDao = new MessageDao(cursor);
         TextView name = (TextView) view.findViewById(R.id.name);
-        name.setText(messageDao.getSenderId()+" ");
+        name.setText(messageDao.getSenderId() + " ");
         TextView message = (TextView) view.findViewById(R.id.message);
         message.setText(messageDao.getText());
+        View indicator = view.findViewById(R.id.unread_indicator);
+        if (!messageDao.isRead()) indicator.setVisibility(View.VISIBLE);
+        else indicator.setVisibility(View.INVISIBLE);
     }
 }
