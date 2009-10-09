a7f1683feaf7f5d727404e669d09c6bb0c07f5a2
ildar.karimov <ildar.karimov@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2009-10-09 22:34:30 +0000

    improved statuses design; added time

--- trunk/src/org/googlecode/vkontakte_android/UpdatesListAdapter.java
+++ trunk/src/org/googlecode/vkontakte_android/UpdatesListAdapter.java
@@ -7,11 +7,13 @@ import android.widget.ImageView;
 import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
 import org.googlecode.vkontakte_android.database.StatusDao;
-import org.googlecode.vkontakte_android.database.UserDao;
+
+import java.text.SimpleDateFormat;
 
 
 public class UpdatesListAdapter extends ResourceCursorAdapter {
     private Context context;
+    public static final SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm ");//todo: get rid of extra space by using padding(?)
 
     public UpdatesListAdapter(Context context, int layout, Cursor cursor) {
         super(context, layout, cursor);
@@ -25,6 +27,8 @@ public class UpdatesListAdapter extends ResourceCursorAdapter {
         nameLine.setText(status.getUserName());
         TextView statusLine = (TextView) view.findViewById(R.id.status);
         statusLine.setText(status.getText());
+        TextView timeLine = (TextView) view.findViewById(R.id.time);
+        timeLine.setText(timeFormat.format(status.getDate()));
         //todo: load user's avatar
         ImageView photo = (ImageView) view.findViewById(R.id.photo);
     }
