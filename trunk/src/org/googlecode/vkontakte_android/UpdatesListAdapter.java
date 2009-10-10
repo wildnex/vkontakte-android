7f15399ab587fadf03add7090301831beab29c0d
ildar.karimov <ildar.karimov@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2009-10-10 16:39:35 +0000

    added photos to statuses

--- trunk/src/org/googlecode/vkontakte_android/UpdatesListAdapter.java
+++ trunk/src/org/googlecode/vkontakte_android/UpdatesListAdapter.java
@@ -2,12 +2,14 @@ package org.googlecode.vkontakte_android;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
 import org.googlecode.vkontakte_android.database.StatusDao;
 
+import java.io.FileNotFoundException;
 import java.text.SimpleDateFormat;
 
 
@@ -27,7 +29,12 @@ public class UpdatesListAdapter extends ResourceCursorAdapter {
         statusLine.setText(status.getText());
         TextView timeLine = (TextView) view.findViewById(R.id.time);
         timeLine.setText(timeFormat.format(status.getDate()));
-        //todo: load user's avatar
-        ImageView photo = (ImageView) view.findViewById(R.id.photo);
+        try {
+            Bitmap bm = UserHelper.getPhotoByUserId(context, status.getUserId());
+            ImageView photo = (ImageView) view.findViewById(R.id.photo);
+            photo.setImageBitmap(bm);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
