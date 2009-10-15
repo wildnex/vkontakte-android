5d6cef0c6e47823ae426480d5ba21c4a57fd884c
serg.podtynnyi <serg.podtynnyi@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2009-10-15 12:21:45 +0000

    * added new strings * moved all initialization stuff to Adapter * all strings on home grid now from R * every grid item is tagged now

--- trunk/src/org/googlecode/vkontakte_android/HomeGridAdapter.java
+++ trunk/src/org/googlecode/vkontakte_android/HomeGridAdapter.java
@@ -14,17 +14,42 @@ import android.widget.TextView;
 
 public class HomeGridAdapter extends BaseAdapter {
 
-	protected ArrayList<String> cell_titles ;
+	protected ArrayList<Integer> cell_titles ;
 	protected ArrayList<Integer> cell_images ;
 	private Context context;
 
 	
-	public HomeGridAdapter(Context context,ArrayList<String> _cell_titles, ArrayList<Integer> _cell_images ) {
+	public HomeGridAdapter(Context context,ArrayList<Integer> _cell_titles, ArrayList<Integer> _cell_images ) {
 		this.cell_titles=_cell_titles;
 		this.cell_images=_cell_images;
 	    this.context = context;
 	}
-	
+	public HomeGridAdapter(Context context ) {
+		
+		cell_titles= new ArrayList<Integer>();
+		cell_titles.add(R.string.my_profile);
+		cell_titles.add(R.string.friends);
+		cell_titles.add(R.string.messages);
+		cell_titles.add(R.string.photos);
+		cell_titles.add(R.string.updates);
+		cell_titles.add(R.string.requests);
+		cell_titles.add(R.string.search);
+		cell_titles.add(R.string.settings);
+		cell_titles.add(R.string.help);
+
+		cell_images= new ArrayList<Integer>();
+		cell_images.add(R.drawable.my_profile);
+		cell_images.add(R.drawable.my_friends);
+		cell_images.add(R.drawable.my_messages);
+		cell_images.add(R.drawable.my_photos);
+		cell_images.add(R.drawable.my_updates);
+		cell_images.add(R.drawable.my_requests);
+		cell_images.add(R.drawable.my_search);
+		cell_images.add(R.drawable.my_settings);
+		cell_images.add(R.drawable.my_help);		
+
+		this.context = context;
+	}
 	
 	@Override
 	public int getCount() {
@@ -35,7 +60,7 @@ public class HomeGridAdapter extends BaseAdapter {
 	@Override
 	public Object getItem(int position) {
 		// TODO Auto-generated method stub
-		if (position<getCount())return this.cell_titles.get(position);
+		if (position<getCount())return context.getResources().getString(this.cell_titles.get(position));
 		return "not cell";
 	}
 
@@ -59,7 +84,7 @@ public class HomeGridAdapter extends BaseAdapter {
 		
 		
 		ly.setBackgroundDrawable(dr);
-		
+		ly.setTag(getItem(position));
 		return ly;
 	}
 
