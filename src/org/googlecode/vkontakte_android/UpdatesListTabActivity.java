4fa26726b13e877d3cbc76f48b749cf78e0eb00a
ildar.karimov <ildar.karimov@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2009-10-16 22:26:30 +0000

    fixed statuses->show profile

--- src/org/googlecode/vkontakte_android/UpdatesListTabActivity.java
+++ src/org/googlecode/vkontakte_android/UpdatesListTabActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import org.googlecode.vkontakte_android.database.StatusDao;
+import org.googlecode.vkontakte_android.database.UserDao;
 import static org.googlecode.vkontakte_android.provider.UserapiDatabaseHelper.*;
 import static org.googlecode.vkontakte_android.provider.UserapiProvider.STATUSES_URI;
 
@@ -34,12 +36,14 @@ public class UpdatesListTabActivity extends ListActivity implements AdapterView.
     public boolean onContextItemSelected(MenuItem item) {
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
         long rowId = info.id;
+        StatusDao status = StatusDao.get(this, rowId);
+        long userId = status.getUserId();
         switch (item.getItemId()) {
             case R.id.view_profile:
-                UserHelper.viewProfile(this, rowId);
+                UserHelper.viewProfile(this, userId);
                 return true;
             case R.id.send_message:
-                UserHelper.sendMessage(this, rowId);
+                UserHelper.sendMessage(this, userId);
                 return true;
             default:
                 return super.onContextItemSelected(item);
@@ -48,6 +52,7 @@ public class UpdatesListTabActivity extends ListActivity implements AdapterView.
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long rowId) {
-        UserHelper.viewProfile(this, rowId);
+        StatusDao status = StatusDao.get(this, rowId);
+        UserHelper.viewProfile(this, status.getUserId());
     }
 }
