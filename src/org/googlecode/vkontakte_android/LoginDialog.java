45bfc63b7d1117c2d779f57b44f09d65e4961b82
jecklandin <jecklandin@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2009-10-21 21:24:13 +0000

    Email checking in the login dialog

--- src/org/googlecode/vkontakte_android/LoginDialog.java
+++ src/org/googlecode/vkontakte_android/LoginDialog.java
@@ -1,15 +1,22 @@
 package org.googlecode.vkontakte_android;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import android.app.Dialog;
 import android.content.Context;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import org.googlecode.vkontakte_android.R;
 
 public class LoginDialog extends Dialog {
-    private Button button;
+    public static final String TAG = "LoginDialog";
+	
+	private Button button;
     private Button cancel;
 
     public LoginDialog(Context context) {
@@ -42,4 +49,35 @@ public class LoginDialog extends Dialog {
     public void stopProgress() {
     	((ProgressBar)findViewById(R.id.progress_bar)).setVisibility(View.GONE);
     }
+    
+    public void showErrorMessage(String err) {
+    	hideErrorMessage();
+    	TextView mess = (TextView)findViewById(R.id.login_error); 
+    	mess.setText(err);
+    	mess.setVisibility(View.VISIBLE);
+    }
+    
+    public void hideErrorMessage() {
+    	((TextView)findViewById(R.id.login_error)).setVisibility(View.GONE);
+    }
+
+	public boolean checkCorrectInput(String login, String pass) {
+		//Everybody stand back! I know regular expressions! 
+	    Pattern pattern = Pattern.compile(
+		"^\\w{1}+[\\w\\d\\.-]*@[\\w\\d]+[\\.\\w\\d-]*\\.[\\w&&[^\\-]]{2,7}$");
+	        
+	    Matcher matcher = pattern.matcher(login);
+	    if (matcher.matches()) {
+        	Log.d(TAG, "Email format checked");	
+        	if (!TextUtils.isEmpty(pass)) {
+        		return true;
+        	}
+        	showErrorMessage("Password shouldn't be empty");
+        	return false;
+        } else {
+        	showErrorMessage("Wrong email format");
+        	Log.d(TAG, "Email wrong format");
+        	return false;
+        }
+	}
 }
