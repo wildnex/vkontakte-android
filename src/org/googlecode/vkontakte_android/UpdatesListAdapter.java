8158811a2aae5b2348efa726c725c80f332b6c73
ildar.karimov <ildar.karimov@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2009-10-22 22:36:48 +0000

    photos in statuses are cropped and resized now

--- src/org/googlecode/vkontakte_android/UpdatesListAdapter.java
+++ src/org/googlecode/vkontakte_android/UpdatesListAdapter.java
@@ -3,8 +3,10 @@ package org.googlecode.vkontakte_android;
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.text.Html;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
@@ -12,6 +14,7 @@ import org.googlecode.vkontakte_android.database.StatusDao;
 
 import java.io.FileNotFoundException;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 
 public class UpdatesListAdapter extends ResourceCursorAdapter {
@@ -31,9 +34,16 @@ public class UpdatesListAdapter extends ResourceCursorAdapter {
         TextView timeLine = (TextView) view.findViewById(R.id.time);
         timeLine.setText(timeFormat.format(status.getDate()));
         Bitmap bm = UserHelper.getPhotoByUserId(context, status.getUserId());
-		if (bm!=null) {
-			ImageView photo = (ImageView) view.findViewById(R.id.photo);
-		    photo.setImageBitmap(bm);	
-		}
+        if (bm != null) {
+            ImageView photo = (ImageView) view.findViewById(R.id.photo);
+            int maxSize = 100;//todo!
+            int srcWidth = bm.getWidth();
+            int srcHeight = bm.getHeight();
+            int dstWidth = maxSize;
+            int dstHeight = srcHeight * maxSize / srcWidth;
+            Bitmap scaledBitmap = Bitmap.createScaledBitmap(bm, dstWidth, dstHeight, false);
+            Bitmap cropped = Bitmap.createBitmap(scaledBitmap, 0, 0, Math.min(maxSize, dstWidth), Math.min(maxSize, dstHeight));
+            photo.setImageBitmap(cropped);
+        }
     }
 }
