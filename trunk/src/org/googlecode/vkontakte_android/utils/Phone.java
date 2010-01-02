ff9438a3fb20441cef6e35f04423d7cdba127cde
anastasiya.dremina <anastasiya.dremina@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2010-01-02 19:24:30 +0000

    result of replacement is used now

--- trunk/src/org/googlecode/vkontakte_android/utils/Phone.java
+++ trunk/src/org/googlecode/vkontakte_android/utils/Phone.java
@@ -2,12 +2,10 @@ package org.googlecode.vkontakte_android.utils;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
- 
-import android.util.Log;
-                 
+
 public class Phone {
     public static String formatPhoneNumber(String number) {
-    	number.replaceAll("[-()]*", "");
+    	number = number.replaceAll("[-()]*", "");
         Pattern pattern = Pattern.compile("(\\+?\\d*).*");
         Matcher matcher = pattern.matcher(number);
         return matcher.find() ? matcher.group(1) : null;
