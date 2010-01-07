856f4cba4a57535d5707c828da47b020cfd3ad3e
ayzen.quwe <ayzen.quwe@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2010-01-07 20:00:47 +0000

    Thread leak fix

--- src/org/googlecode/vkontakte_android/service/UpdatesNotifier.java
+++ src/org/googlecode/vkontakte_android/service/UpdatesNotifier.java
@@ -5,8 +5,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Looper;
-import android.widget.Toast;
+/*import android.os.Looper;
+import android.widget.Toast;*/
 import org.googlecode.vkontakte_android.CGuiTest;
 import org.googlecode.vkontakte_android.ComposeMessageActivity;
 import org.googlecode.vkontakte_android.HomeGridActivity;
@@ -18,73 +18,64 @@ import org.googlecode.vkontakte_android.provider.UserapiDatabaseHelper;
 class UpdatesNotifier {
 
     public static void showError(final Context ctx, final int error) {
-        new Thread() {
+        // I don't think it's a good idea to use toast notification about errors from services. Normally that should be
+        // done from activities. If every application would use toasts from services, then in case of network problems
+        // user will get dozen of toasts with "network problem" notification.
+
+        // Here we got thread leak: every showError() usage will create thread and Looper.loop() will hang that thread.
+        //TODO: use Handler or use this method only from UI threads
+        /*new Thread() {
             @Override
             public void run() {
                 Looper.prepare();
                 Toast.makeText(ctx, error, Toast.LENGTH_SHORT).show();
                 Looper.loop();
             }
-        }.start();
+        }.start();*/
     }
 
     public static void notifyHistoryMessages(final Context ctx, final long friends, final long mess, final long tags) {
-        new Thread() {
-            @Override
-            public void run() {
-                Looper.prepare();
-                NotificationManager mNotificationManager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
-                String titleText = "You have received: ";
+        NotificationManager mNotificationManager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+        String titleText = "You have received: ";
 
-                String f = (friends != 0) ? "Fr: " + friends : "";
-                String m = (mess != 0) ? " Mess: " + mess : "";
-                String t = (tags != 0) ? " Tags: " + tags : "";
+        String f = (friends != 0) ? "Fr: " + friends : "";
+        String m = (mess != 0) ? " Mess: " + mess : "";
+        String t = (tags != 0) ? " Tags: " + tags : "";
 
-                String text = f + m + t;
+        String text = f + m + t;
 
-                Notification notification = new Notification(R.drawable.my_help, "VKontakte: updates", System.currentTimeMillis());
-                Intent notificationIntent = new Intent(ctx, HomeGridActivity.class);
-                PendingIntent contentIntent = PendingIntent.getActivity(ctx, 0, notificationIntent, 0);
-                notification.setLatestEventInfo(ctx, titleText, text, contentIntent);
-                final int VK_ID = 42;
-                mNotificationManager.notify(VK_ID, notification);
-
-                Looper.loop();
-            }
-        }.start();
+        Notification notification = new Notification(R.drawable.my_help, "VKontakte: updates", System.currentTimeMillis());
+        Intent notificationIntent = new Intent(ctx, HomeGridActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(ctx, 0, notificationIntent, 0);
+        notification.setLatestEventInfo(ctx, titleText, text, contentIntent);
+        final int VK_ID = 42;
+        mNotificationManager.notify(VK_ID, notification);
     }
 
     public static void notifyMessages(final Context ctx, final long num, final MessageDao mess) {
         if (num < 1) return;
 
-        new Thread() {
-            @Override
-            public void run() {
-                Looper.prepare();
-                NotificationManager mNotificationManager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
-                String tickerText = "VKontakte updates";
-                Notification notification = new Notification(R.drawable.icon, tickerText, System.currentTimeMillis());
-                String contentTitle;
-                String contentText = "";
-                Intent notificationIntent;
-                if (num == 1) {
-                    contentTitle = "New message from " + mess.getSender(ctx).userName;
-                    contentText = mess.text;
-                    notificationIntent = new Intent(ctx, ComposeMessageActivity.class)
-                            .putExtra(UserapiDatabaseHelper.KEY_MESSAGE_SENDERID, mess.senderId);
-                } else {
-                    contentTitle = num + " new messages.";
-                    notificationIntent = new Intent(ctx, CGuiTest.class).putExtra("tabToShow", "Messages").setFlags(
-                            Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                }
-                PendingIntent contentIntent = PendingIntent.getActivity(ctx, 0, notificationIntent, 0);
-                notification.setLatestEventInfo(ctx, contentTitle, contentText, contentIntent);
-                notification.flags |= Notification.FLAG_AUTO_CANCEL;
-                final int HELLO_ID = 2;
-                mNotificationManager.notify(HELLO_ID, notification);
-                Looper.loop();
-            }
-        }.start();
+        NotificationManager mNotificationManager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+        String tickerText = "VKontakte updates";
+        Notification notification = new Notification(R.drawable.icon, tickerText, System.currentTimeMillis());
+        String contentTitle;
+        String contentText = "";
+        Intent notificationIntent;
+        if (num == 1) {
+            contentTitle = "New message from " + mess.getSender(ctx).userName;
+            contentText = mess.text;
+            notificationIntent = new Intent(ctx, ComposeMessageActivity.class)
+                    .putExtra(UserapiDatabaseHelper.KEY_MESSAGE_SENDERID, mess.senderId);
+        } else {
+            contentTitle = num + " new messages.";
+            notificationIntent = new Intent(ctx, CGuiTest.class).putExtra("tabToShow", "Messages").setFlags(
+                    Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        }
+        PendingIntent contentIntent = PendingIntent.getActivity(ctx, 0, notificationIntent, 0);
+        notification.setLatestEventInfo(ctx, contentTitle, contentText, contentIntent);
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+        final int HELLO_ID = 2;
+        mNotificationManager.notify(HELLO_ID, notification);
     }
 
 }
