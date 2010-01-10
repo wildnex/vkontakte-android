10e41b5f34c77ed2b82ddb2f8d48386c9deda432
serg.podtynnyi <serg.podtynnyi@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2010-01-10 22:58:14 +0000

    * optimizing photos in all activities

--- src/org/googlecode/vkontakte_android/FriendsListAdapter.java
+++ src/org/googlecode/vkontakte_android/FriendsListAdapter.java
@@ -1,12 +1,8 @@
 package org.googlecode.vkontakte_android;
 
-import java.lang.ref.SoftReference;
-import java.util.HashMap;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ResourceCursorAdapter;
@@ -18,11 +14,10 @@ import org.googlecode.vkontakte_android.utils.UserHelper;
 
 public class FriendsListAdapter extends ResourceCursorAdapter {
     private static final String TAG = "VK:FriendsListAdapter";
-    private HashMap<Long, SoftReference<Bitmap>> bitmapCache = null;
+    
 
     public FriendsListAdapter(Context context, int layout, Cursor cursor) {
     	super(context, layout, cursor);
-    	bitmapCache = new HashMap<Long, SoftReference<Bitmap>>();
     }
 
     @Override
@@ -43,29 +38,7 @@ public class FriendsListAdapter extends ResourceCursorAdapter {
         ImageView photo = (ImageView) view.findViewById(R.id.photo);
         
         if (Settings.shouldLoadPics(context)) {
-            if (userDao.getUserPhotoUrl() != null) {
-     
-            	Bitmap bm=null;
-            	SoftReference<Bitmap> photoRef = bitmapCache.get(userDao.rowId);
-            	if (photoRef != null) {
-            	    bm = photoRef.get();
-            	}
-            	if (bm==null){
-            		bitmapCache.remove(userDao.rowId);
-            		bm=UserHelper.getPhotoByUser(context, userDao);
-            		if (bm!=null){
-            			bitmapCache.put(userDao.rowId, new SoftReference<Bitmap>(bm));	
-                        photo.setImageBitmap(bitmapCache.get(userDao.rowId).get());
-            		}else{
-            			photo.setImageBitmap(CImagesManager.getBitmap(context, Icons.STUB));
-            		}
-            	}else{
-            		photo.setImageBitmap(bm);
-            	}
-            } else {
-                Log.e(TAG, "Error: no photo url for " + userDao.userName);
-                photo.setImageBitmap(CImagesManager.getBitmap(context, Icons.STUB));
-            }
+        	photo.setImageBitmap(UserHelper.getPhotoByUser(context, userDao));
         } else {
             photo.setImageBitmap(CImagesManager.getBitmap(context, Icons.STUB));
             photo.setVisibility(View.GONE);
