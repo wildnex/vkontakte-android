c77ebece2860b7a40e6fe257f5dbd419e95205df
jecklandin <jecklandin@1fbf2240-90ee-11de-82e5-a1837a67dc72>
2010-02-16 00:03:46 +0000

    Added logging class

--- src/org/googlecode/vkontakte_android/service/MyRemoteException.java
+++ src/org/googlecode/vkontakte_android/service/MyRemoteException.java
@@ -2,6 +2,9 @@ package org.googlecode.vkontakte_android.service;
 
 import android.os.RemoteException;
 
+/**
+ *  Used for encapsulation of non-remote exception for throwing them from the service. 
+ */
 public class MyRemoteException extends RemoteException {
     private static final long serialVersionUID = 1L;
     public Exception innerException;
